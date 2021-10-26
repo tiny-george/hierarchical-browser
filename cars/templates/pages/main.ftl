c642d81532bffc627ea9f55ec858ad72cfc87e8c
Jorge Franco <jorge.franco@magnolia-cms.com>
2021-10-26 09:04:44 +0200

    first commit

---
+++ cars/templates/pages/main.ftl
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <script type="module" src="${ctx.contextPath}/.resources/cars/webresources/delivery-client.js"></script>
+    <script type="module" src="${ctx.contextPath}/.resources/cars/webresources/hierarchical-browser.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+        p {
+            color: green;
+        }
+    </style>
+</head>
+<body>
+    <p>Testing web component <span>hierarchical-browser</span></p>
+
+    <template id="browser-loading">
+        <style>
+            span {
+                color: grey;
+            }
+        </style>
+        <p>Loading browser for endpoint <span class="endpoint-name"></span> ...</p>
+    </template>
+    <template id="browser-items">
+        <style>
+            .content-list {
+                width: 100%;
+            }
+            thead {
+                font-weight: bolder;
+                text-transform: capitalize;   
+            }
+            .folder {
+                background-color: yellow;
+            }
+        </style>
+        <p>Showing items in path <span class="items-path"></span></p>
+        <table class="content-list">
+        </table>
+    </template>
+
+    <hierarchical-browser baseUrl="${ctx.contextPath}" endpoint="cars" columns="name,model,power"></hierarchical-browser>
+</body>
+</html>
---
+++ cars/webresources/hierarchical-browser.js
@@ -0,0 +1,195 @@
+var getClient = function getClient(baseUrl, endpointName) {
+  return {
+    baseUrl: baseUrl,
+    endpointName: endpointName,
+    getItems: function getItems(path) {
+      return clientFunctions.makeCall(this.baseUrl + "/.rest/delivery/" + endpointName + path + "@nodes").then(function (data) {
+        return JSON.parse(data);
+      }).then(function (json) {
+        return json.map(jsonToItem);
+      });
+    }
+  };
+};
+
+var jsonToItem = function jsonToItem(data) {
+  var map = new Map(Object.entries(data)).set('isFolder', data['@nodeType'] === "mgnl:folder");
+  map["delete"]('@nodes');
+  map["delete"]('@nodeType');
+  map["delete"]('@name');
+  return map;
+};
+
+var makeCall = function makeCall(url) {
+  return new Promise(function (resolve, reject) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onload = function () {
+      if (request.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(Error('Error making call to ' + url + '; error code:' + request.statusText));
+      }
+    };
+
+    request.onerror = function () {
+      reject(Error('There was a network error making call to ' + url));
+    };
+
+    request.send();
+  });
+};
+
+var clientFunctions = {
+  makeCall: makeCall,
+  getClient: getClient
+};
+
+export default class HierarchicalBrowser extends HTMLElement {
+    constructor() {
+        super();
+        this.baseUrl = this.getAttribute("baseUrl");
+        this.endpoint = this.getAttribute("endpoint");
+        const columns = this.getAttribute("columns");
+        if (!columns) {
+          this.columns = ["@path", "@name"];
+        } else {
+          this.columns = columns.split(","); 
+        }
+        this.sh = this.attachShadow({mode: 'open'});
+        this.client = getClient(this.baseUrl, this.endpoint);
+        this.loadItems('/');
+    }
+
+    loadItems(path) {
+      console.log("Go path ->" + path);
+      if (path != "/") {
+        let parent = path.substring(0, path.length - 1);
+        const lastSlash = parent.lastIndexOf('/');
+        this.parent = lastSlash == 0 ? "/" : parent.substring(0, lastSlash);
+      } else {
+        this.parent = null;
+      }
+      this.path = path;
+      this.state = "loading";
+      this.client.getItems(path).then(result => {
+        this.items = result;
+        this.state = 'ready';
+      }).catch(error => {
+        this.errorMessage = error.name + ": " + error.message;
+        this.state = 'error';
+      });
+    }
+
+    get state() {
+        return this.getAttribute('state');
+    }
+
+    set state(value) {
+        this.setAttribute('state', value);
+    }
+
+    static get observedAttributes() { return ['state']; }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+      this.render();
+   }
+
+    connectedCallback() {
+      this.render();
+    }
+
+    render() {
+        this.sh.innerHTML = '';
+        if (this.getAttribute('state') == "loading") {
+            this.sh.appendChild(this.loading());
+        }
+        if (this.getAttribute('state') == "ready") {
+          this.sh.appendChild(this.showItems());
+        }
+        if (this.getAttribute('state') == "error") {
+            this.sh.innerHTML = '<p class="error">' + this.errorMessage || "Unknown Error" + '<p>';
+        }
+    }
+
+    loading() {
+        const template = document.getElementById('browser-loading');
+        const instance = document.importNode(template.content, true);
+        instance.querySelector('.endpoint-name').innerHTML = this.endpoint;
+        return instance;
+    }
+
+    showItems() {
+      const template = document.getElementById('browser-items');
+      const instance = document.importNode(template.content, true);
+      instance.querySelector('.items-path').innerHTML = this.path;
+      instance.querySelector('.content-list').innerHTML = this.getItemsHtml();
+      this.addFolderClickEvent(instance);
+      return instance;
+    }
+
+    getItemsHtml() {
+      if (this.items.size < 1) {
+        return "<thead><tr><th>empty</th></tr></thead>"
+      } else {
+        let result = "";
+        if (this.parent != null) {
+          result += '<thead><tr><td class="folder" folder="' + this.parent + '">Go to ' + this.parent + '</td></tr></thead>';
+        }
+        result += this.getItemsHeader(this.columns) + this.getItemsBody(this.items, this.columns);
+        return result;
+      }
+    }
+
+    getItemsHeader(columns) {
+      let headers = "";
+      columns.forEach(column => headers += "<td>" + column + "</td>");
+      return "<thead><tr>" + headers + "</tr></thead>";
+    }
+
+    getItemsBody(items, columns) {
+      let rows = "";
+      items.forEach(content => {
+        let row = "<tr>";
+        if (content.get("isFolder")) {
+          row = '<tr class="folder" folder="' + content.get('@path') + '">'
+        }
+        columns.forEach(column => {
+          const value = content.get(column);
+          if (value == undefined) {
+            row = row + "<td></td>";
+          } else {
+            row = row + "<td>" + content.get(column) + "</td>";
+          }
+        });
+        row = row + "</tr>";
+        rows = rows + row;
+      });
+      return "<tbody>" + rows + "</tbody>";     
+    }
+
+    addFolderClickEvent(instance) {
+      if (instance.querySelector('.folder')) {
+        const that = this;
+        const elements = instance.querySelectorAll('.folder');
+        elements.forEach(element => {
+          element.addEventListener('click', function(event) {
+            event.preventDefault();
+            let row = event.target;
+            while (row.getAttribute("folder") == undefined) {
+              row = row.parentElement;
+            }
+            let path = row.getAttribute("folder");
+            if (!path.endsWith("/")) {
+              path = path + "/";
+            }
+            that.loadItems(path);
+          });
+        });
+      }      
+    } 
+}
+
+if (!customElements.get('hierarchical-browser')) {
+    customElements.define('hierarchical-browser', HierarchicalBrowser);
+}
